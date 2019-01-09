ecd53c2bf4b895bf8adab13edac09b0ec26861fd
Alexander Weir <CryptoPynchon@gmail.com>
2019-01-09 12:57:02 -0500

    bjork

--- app/assets/javascripts/ping.js
+++
@@ -1,7 +0,0 @@
-console.log("As the universe grows more complex, it seems I should too.")
-
-var http = require("http");
-
-setInterval(function() {
-      http.get("http://www.hedononoia.herokuapp.com");
-}, 1740000);
